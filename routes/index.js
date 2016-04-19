2a1b96034f89c8d2eefeba7d8326041bb6cbc433
xalava <xavier.lavayssiere@gmail.com>
2016-04-19 06:12:45 +0200

    mise en forme

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,17 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { titre: 'BePulse' });
 });
 
+router.get('/profile', function(req, res, next) {
+  res.render('profile', { titre: 'BePulse - profile' });
+});
+
+router.get('/admin', function(req, res, next) {
+  res.render('admin', { titre: 'BePulse - Admin Panel' });
+});
+
+
 module.exports = router;
+
